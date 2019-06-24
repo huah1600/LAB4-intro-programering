ada25a9ec0e155fc3e335f52bd2987393938175a
abullahi <49534483+huah1600@users.noreply.github.com>
2019-06-24 19:50:08 +0200

    enkel exempel på hur man använder function

---
+++ function2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void oscar_wilde_quote(){ // det här är defintion,vilket innebär att det instruktioner den hååler på med.
+  std::cout<<"The highest, as the lowest, form of criticism is a mode of autobiography."<<"\n";
+}
+
+
+int main() {
+  
+ oscar_wilde_quote();// kallar på funktionen void oscar//calling,det här är deklaritone
+  
+}
