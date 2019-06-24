0c0e38bf193180a6f6d7a356ce72750ac186de84
abullahi <49534483+huah1600@users.noreply.github.com>
2019-06-24 21:56:33 +0200

    hur jag använde return

---
+++ function7.cpp
@@ -0,0 +1,25 @@
+
+#include<iostream>
+#include<string>
+                //        
+std::string my_name() {
+
+	std::string namo = "husse";
+	
+	
+
+	return namo;
+}
+
+int main()
+{
+
+
+
+std::cout<<"are your name "<<my_name(); // calling the function,thats your power.
+
+
+	std::cin.get();
+
+	return 0;
+}
