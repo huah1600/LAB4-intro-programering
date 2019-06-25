7a5e7ee22762b287b596921348d4f23e7c11285a
abullahi <49534483+huah1600@users.noreply.github.com>
2019-06-25 14:22:48 +0200

    hur man man printar ut return double i funktion

--- function11.cpp
+++ function11.cpp
@@ -13,3 +13,22 @@ int main() {
   std::cout << average(1.0, 2.0) << "\n";
   
 }
+
+ett annant sätt
+
+
+#include <iostream>
+
+// Define average() here:
+double average(double num1,double num2){
+  double avg= (num1+num2)/(2);
+  std::cout<<"medelvärde"<<avg<<"\n";  <<<  // skriver ut hela svaret inne i defintionen.
+ return avg; ----------------------------|
+}
+
+int main() {
+  
+ average(42.0, 24.0);
+ average(1.0, 2.0);
+  
+}
