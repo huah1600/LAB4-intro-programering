8aa58d34d39ec9038d689a5cb842e046f79c4fdb
abullahi <49534483+huah1600@users.noreply.github.com>
2019-06-29 15:25:49 +0200

    förståd

---
+++ LAb4_main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include "source1.h"
+#include  <fstream>
+using namespace std;
+
+int main()
+{
+	
+
+	
+	
+		
+		sortA1();
+		sortA();
+		sortB();
+		merge();
+
+
+
+	system("pause");
+	return 0;
+}
